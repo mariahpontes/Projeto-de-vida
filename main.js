8013db811273395f8e8a0f1d31f0ed2202d8d3ef
mariahpontes <lauany.luz@escola.pr.gov.br>
2025-06-12 11:02:47 -0300

    salvar

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-11-09T00:00:00");
 const tempoObjetivo2 = new Date("2026-01-01T00:00:00");
-const tempoObjetivo3 = new Date("2025-09-30T00:00:00");
-const tempoObjetivo4 = new Date("2032-02-01T00:00:00");
+const tempoObjetivo3 = new Date("2025-09-27T00:00:00");
+const tempoObjetivo4 = new Date("2035-04-27T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
